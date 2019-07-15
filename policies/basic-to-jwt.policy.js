a23fcf7f60a0e27462522d4a7fe489ddb20c860a
BriannaGreene <hellobrigreene@gmail.com>
2019-07-15 10:46:49 -0600

    [LZR-63] updated auth

--- policies/basic-to-jwt.policy.js
+++ policies/basic-to-jwt.policy.js
@@ -13,17 +13,18 @@ module.exports = {
   },
   policy: ({ authUrl }) => {
     return async (req, res, next) => {
-      console.log('Executing "basic-to-jwt" policy with "authServiceUrl" of', authUrl);
       try {
-        const token = await request({
-          uri: authUrl,
-          headers: req.headers,
-        });
-        req.headers = {
-          ...req.headers,
-          authorization: `Bearer ${token}`,
-        }
-        console.log(req.headers)
+				const authHeader = (req.headers || {}).authorization;
+				if (authHeader && authHeader.startsWith('Basic ')) {
+					const token = await request({
+						uri: authUrl,
+						headers: req.headers,
+					});
+					req.headers = {
+						...req.headers,
+						authorization: `Bearer ${token}`,
+					}
+				}
       } catch (e) {
         console.error('Request to auth service failed. Error:', e.error)
         res.status(e.statusCode).send(e.message)
